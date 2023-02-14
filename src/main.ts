40dabc3b05167531159cffc5d523b82dc0236dfc
Sokleng <96036270+SoklengBun@users.noreply.github.com>
2023-02-14 18:35:21 +0700

    ad i18n (#3)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
+import { i18n } from "./locales";
 import App from "./App.vue";
 import router from "./router";
 import { Popup, Switch } from "vant";
@@ -13,6 +13,7 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(i18n);
 app.use(Popup);
 app.use(Switch);
 
