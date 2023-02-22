7b47eae2bc4380f5be749235b9c8e03385a80c9d
Sokleng Bun <soklengbun22@gmail.com>
2023-02-22 15:52:28 +0700

    fix stuff

--- src/components/pages/home/components/slides/SecondSlide.vue
+++ src/components/pages/home/components/slides/SecondSlide.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts"></script>
 <template>
   <section class="item h-screen overflow-hidden">
-    <div id="my-id" class="background-i first-slide flex h-full w-full"></div>
+    <div id="my-id" class="background-i second-slide flex h-full w-full"></div>
   </section>
 </template>
 <style>
-.background-i.first-slide {
-  background-image: url('https://redive.estertion.win/card/full/124131.webp');
+.background-i.second-slide {
+  background-image: url('https://redive.estertion.win/card/full/124531.webp');
   background-repeat: no-repeat;
   background-size: auto 120vh;
   opacity: 0.5;
