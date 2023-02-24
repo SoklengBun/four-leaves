dc2fb1060e5615aef8749fc8e6a7353c28cd4706
Sokleng <96036270+SoklengBun@users.noreply.github.com>
2023-02-24 18:09:18 +0700

    fix opacity slide background (#12)

--- src/components/pages/home/components/slides/FirstSlide.vue
+++ src/components/pages/home/components/slides/FirstSlide.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts"></script>
 <template>
-  <section class="item h-screen overflow-hidden">
-    <div id="my-id" class="background-i first-slide flex h-full w-full">
-      <div class="relative h-full w-1/2">
+  <section class="item relative h-screen overflow-hidden">
+    <div class="background-i first-slide absolute top-0 h-full w-full" />
+    <div id="my-id" class="flex h-full w-full flex-col md:flex-row">
+      <div class="relative w-full flex-1 !opacity-100 md:h-full md:w-1/2">
         <div class="slide-left absolute bottom-0 left-0 w-full bg-black p-5">
           hey
         </div>
       </div>
-      <div class="relative h-full w-1/2">
+      <div class="relative w-full flex-1 md:h-full md:w-1/2">
         <div class="slide-right absolute bottom-0 right-0 w-full p-5">
           heyaa
         </div>
@@ -23,7 +24,8 @@
   background-size: auto 120vh;
   opacity: 0.5;
 }
-@media (min-width: 700px) {
+
+@media (min-width: 768px) {
   .slide-right {
     transition: transform 330ms;
     transition-timing-function: ease-in-out;
@@ -37,6 +39,9 @@
   }
 
   .show .slide-left {
+    transform: translate3d(0, 0, 0);
+  }
+  .show .slide-right {
     transform: translate3d(0, 0, 0);
   }
 }
