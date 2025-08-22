ffe443abbd60bb96cc895625203401054f2d3602
Sokleng <soklengbun22@gmail.com>
2025-08-22 13:56:46 +0700

    delay set heigh

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import NavBar from '~/components/layout/header/NavBar.vue';
 import HomePage from '~/components/pages/home/HomePage.vue';
 </script>
 
 <template>
+  <NavBar />
   <HomePage />
 </template>
