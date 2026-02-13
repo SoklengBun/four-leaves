920c14a21a9f974e2c7040c3c386cc1f620937d7
Sokleng <soklengbun22@gmail.com>
2026-02-13 11:16:18 +0700

    fix?

--- src/components/pages/home/components/MobileFrontPage.vue
+++ src/components/pages/home/components/MobileFrontPage.vue
@@ -65,8 +65,10 @@ const tips = ['Break a leg!', "You've got this!", 'Blow them away!', 'Bring home
 
 <style scoped>
 .mobile-front-container {
-  @apply relative h-body w-full p-3 md:hidden;
+  @apply relative h-body w-full px-3 md:hidden;
 
+  padding-top: 12px;
+  padding-top: env(safe-area-inset-top);
   background-color: rgb(3, 9, 35);
 }
 
